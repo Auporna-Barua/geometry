8a81819a91fbf7deb9a8973ed73c6334d13f4d3d
Auporna-Barua <120823697+Auporna-Barua@users.noreply.github.com>
2023-02-18 03:20:39 +0600

    get triangle value and button

---
+++ geometry-genius.js
@@ -0,0 +1,4 @@
+// step-1: Triangle value
+const TriangleBase = document.getElementById("triangleBase").value;
+const TriangleHeight = document.getElementById("triangleHeight").value;
+const TriangleBtn = document.getElementById("triangleBtn");
