2e7d20c555ffd12efedd2238a7e9d7309662b475
Auporna-Barua <120823697+Auporna-Barua@users.noreply.github.com>
2023-02-18 03:26:53 +0600

    get rectangle value and button

--- geometry-genius.js
+++ geometry-genius.js
@@ -2,3 +2,8 @@
 const TriangleBase = document.getElementById("triangleBase").value;
 const TriangleHeight = document.getElementById("triangleHeight").value;
 const TriangleBtn = document.getElementById("triangleBtn");
+
+// step-2: Rectangle value
+const RectangleWidth = document.getElementById("rectangleWidth").value;
+const RectangleLand = document.getElementById("rectangleLand").value;
+const RectangleBtn = document.getElementById("rectangleBtn");
