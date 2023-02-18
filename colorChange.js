92afea07be705854fbda8d5f76229ecb257ff0c2
Auporna-Barua <120823697+Auporna-Barua@users.noreply.github.com>
2023-02-18 14:44:05 +0600

    card background change randomly and result show on calculation math

---
+++ colorChange.js
@@ -0,0 +1,150 @@
+// background color show : card 1
+document.getElementById("bgColor1").addEventListener("mouseover", function () {
+  const number = Math.floor(Math.random() * 10 + 1);
+  if (number === 1) {
+    document.getElementById("bgColor1").style.backgroundColor = "#F16767";
+  } else if (number === 2) {
+    document.getElementById("bgColor1").style.backgroundColor = "#A459D1";
+  } else if (number === 3) {
+    document.getElementById("bgColor1").style.backgroundColor = "#3E54AC";
+  } else if (number === 4) {
+    document.getElementById("bgColor1").style.backgroundColor = "#443C68";
+  } else if (number === 5) {
+    document.getElementById("bgColor1").style.backgroundColor = "#E3DFFD";
+  } else if (number === 6) {
+    document.getElementById("bgColor1").style.backgroundColor = "#537FE7";
+  } else if (number === 7) {
+    document.getElementById("bgColor1").style.backgroundColor = "#B9F3E4";
+  } else if (number === 8) {
+    document.getElementById("bgColor1").style.backgroundColor = "#C92C6D";
+  } else if (number === 9) {
+    document.getElementById("bgColor1").style.backgroundColor = "#85CDFD";
+  } else if (number === 10) {
+    document.getElementById("bgColor1").style.backgroundColor = "#FFD4D4";
+  }
+});
+// background color show : card 2
+document.getElementById("bgColor2").addEventListener("mouseover", function () {
+  const number = Math.floor(Math.random() * 10 + 1);
+  if (number === 1) {
+    document.getElementById("bgColor2").style.backgroundColor = "#F16767";
+  } else if (number === 2) {
+    document.getElementById("bgColor2").style.backgroundColor = "#A459D1";
+  } else if (number === 3) {
+    document.getElementById("bgColor2").style.backgroundColor = "#3E54AC";
+  } else if (number === 4) {
+    document.getElementById("bgColor2").style.backgroundColor = "#443C68";
+  } else if (number === 5) {
+    document.getElementById("bgColor2").style.backgroundColor = "#E3DFFD";
+  } else if (number === 6) {
+    document.getElementById("bgColor2").style.backgroundColor = "#537FE7";
+  } else if (number === 7) {
+    document.getElementById("bgColor2").style.backgroundColor = "#B9F3E4";
+  } else if (number === 8) {
+    document.getElementById("bgColor2").style.backgroundColor = "#C92C6D";
+  } else if (number === 9) {
+    document.getElementById("bgColor2").style.backgroundColor = "#85CDFD";
+  } else if (number === 10) {
+    document.getElementById("bgColor2").style.backgroundColor = "#FFD4D4";
+  }
+});
+// background color show : card 3
+document.getElementById("bgColor3").addEventListener("mouseover", function () {
+  const number = Math.floor(Math.random() * 10 + 1);
+  if (number === 1) {
+    document.getElementById("bgColor3").style.backgroundColor = "#F16767";
+  } else if (number === 2) {
+    document.getElementById("bgColor3").style.backgroundColor = "#A459D1";
+  } else if (number === 3) {
+    document.getElementById("bgColor3").style.backgroundColor = "#3E54AC";
+  } else if (number === 4) {
+    document.getElementById("bgColor3").style.backgroundColor = "#443C68";
+  } else if (number === 5) {
+    document.getElementById("bgColor3").style.backgroundColor = "#E3DFFD";
+  } else if (number === 6) {
+    document.getElementById("bgColor3").style.backgroundColor = "#537FE7";
+  } else if (number === 7) {
+    document.getElementById("bgColor3").style.backgroundColor = "#B9F3E4";
+  } else if (number === 8) {
+    document.getElementById("bgColor3").style.backgroundColor = "#C92C6D";
+  } else if (number === 9) {
+    document.getElementById("bgColor3").style.backgroundColor = "#85CDFD";
+  } else if (number === 10) {
+    document.getElementById("bgColor3").style.backgroundColor = "#FFD4D4";
+  }
+});
+// background color show : card 4
+document.getElementById("bgColor4").addEventListener("mouseover", function () {
+  const number = Math.floor(Math.random() * 10 + 1);
+  if (number === 1) {
+    document.getElementById("bgColor4").style.backgroundColor = "#F16767";
+  } else if (number === 2) {
+    document.getElementById("bgColor4").style.backgroundColor = "#A459D1";
+  } else if (number === 3) {
+    document.getElementById("bgColor4").style.backgroundColor = "#3E54AC";
+  } else if (number === 4) {
+    document.getElementById("bgColor4").style.backgroundColor = "#443C68";
+  } else if (number === 5) {
+    document.getElementById("bgColor4").style.backgroundColor = "#E3DFFD";
+  } else if (number === 6) {
+    document.getElementById("bgColor4").style.backgroundColor = "#537FE7";
+  } else if (number === 7) {
+    document.getElementById("bgColor4").style.backgroundColor = "#B9F3E4";
+  } else if (number === 8) {
+    document.getElementById("bgColor4").style.backgroundColor = "#C92C6D";
+  } else if (number === 9) {
+    document.getElementById("bgColor4").style.backgroundColor = "#85CDFD";
+  } else if (number === 10) {
+    document.getElementById("bgColor4").style.backgroundColor = "#FFD4D4";
+  }
+});
+// background color show : card 5
+document.getElementById("bgColor5").addEventListener("mouseover", function () {
+  const number = Math.floor(Math.random() * 10 + 1);
+  if (number === 1) {
+    document.getElementById("bgColor5").style.backgroundColor = "#F16767";
+  } else if (number === 2) {
+    document.getElementById("bgColor5").style.backgroundColor = "#A459D1";
+  } else if (number === 3) {
+    document.getElementById("bgColor5").style.backgroundColor = "#3E54AC";
+  } else if (number === 4) {
+    document.getElementById("bgColor5").style.backgroundColor = "#443C68";
+  } else if (number === 5) {
+    document.getElementById("bgColor5").style.backgroundColor = "#E3DFFD";
+  } else if (number === 6) {
+    document.getElementById("bgColor5").style.backgroundColor = "#537FE7";
+  } else if (number === 7) {
+    document.getElementById("bgColor5").style.backgroundColor = "#B9F3E4";
+  } else if (number === 8) {
+    document.getElementById("bgColor5").style.backgroundColor = "#C92C6D";
+  } else if (number === 9) {
+    document.getElementById("bgColor5").style.backgroundColor = "#85CDFD";
+  } else if (number === 10) {
+    document.getElementById("bgColor5").style.backgroundColor = "#FFD4D4";
+  }
+});
+// background color show : card 6
+document.getElementById("bgColor6").addEventListener("mouseover", function () {
+  const number = Math.floor(Math.random() * 10 + 1);
+  if (number === 1) {
+    document.getElementById("bgColor6").style.backgroundColor = "#F16767";
+  } else if (number === 2) {
+    document.getElementById("bgColor6").style.backgroundColor = "#A459D1";
+  } else if (number === 3) {
+    document.getElementById("bgColor6").style.backgroundColor = "#3E54AC";
+  } else if (number === 4) {
+    document.getElementById("bgColor6").style.backgroundColor = "#443C68";
+  } else if (number === 5) {
+    document.getElementById("bgColor6").style.backgroundColor = "#E3DFFD";
+  } else if (number === 6) {
+    document.getElementById("bgColor6").style.backgroundColor = "#537FE7";
+  } else if (number === 7) {
+    document.getElementById("bgColor6").style.backgroundColor = "#B9F3E4";
+  } else if (number === 8) {
+    document.getElementById("bgColor6").style.backgroundColor = "#C92C6D";
+  } else if (number === 9) {
+    document.getElementById("bgColor6").style.backgroundColor = "#85CDFD";
+  } else if (number === 10) {
+    document.getElementById("bgColor6").style.backgroundColor = "#FFD4D4";
+  }
+});
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const li = document.createElement("li");
+li.classList =
+  "list-group-item d-flex justify-content-between align-items-start gap-2";
+
 // step-1: Triangle value
 
 document.getElementById("triangleBtn").addEventListener("click", function () {
@@ -15,14 +19,20 @@ document.getElementById("triangleBtn").addEventListener("click", function () {
     alert("Input is required, must be a positve number");
   } else {
     const triangleArea = 0.5 * triangleBaseParse * triangleHeightParse;
-    // console.log(triangleArea);
+    const text = ` <div class="ms-2 me-auto">Triangle</div>
+                <div>${triangleArea}cm<sup>2</sup></div>
+                <button class="btn btn-primary btn-sm">
+                  Convert to m <sup>2</sup>
+                </button>`;
+    li.innerHTML = text;
+    document.getElementById("resultShow").appendChild(li);
   }
 });
 
 // step-2: Rectangle value
 document.getElementById("rectangleBtn").addEventListener("click", function () {
   const rectangleWidth = document.getElementById("rectangleWidth").value;
-   const rectangleWidthParse = parseFloat(rectangleWidth);
+  const rectangleWidthParse = parseFloat(rectangleWidth);
   const rectangleLand = document.getElementById("rectangleLand").value;
   const rectanglenLandParse = parseFloat(rectangleLand);
 
@@ -34,15 +44,26 @@ document.getElementById("rectangleBtn").addEventListener("click", function () {
   ) {
     alert("Input is required, must be a positve number");
   } else {
-    const rectanglenArea =  rectangleWidthParse * rectanglenLandParse;
-    // console.log(rectanglenArea);
-  }
+    const rectanglenArea = (rectangleWidthParse * rectanglenLandParse).toFixed(
+      2
+    );
 
+    const text = ` <div class="ms-2 me-auto">Triangle</div>
+                <div>${rectanglenArea}cm<sup>2</sup></div>
+                <button class="btn btn-primary btn-sm">
+                  Convert to m <sup>2</sup>
+                </button>`;
+    li.innerHTML = text;
+    document.getElementById("resultShow").appendChild(li);
+  }
 });
 
 // step-3: Parallelogram value
-document.getElementById("parallelogramBtn").addEventListener("click", function () {
-    const parallelogramBase = document.getElementById("parallelogramBase").value;
+document
+  .getElementById("parallelogramBtn")
+  .addEventListener("click", function () {
+    const parallelogramBase =
+      document.getElementById("parallelogramBase").value;
     const parallelogramBaseParse = parseFloat(parallelogramBase);
     const parallelogramHeight = document.getElementById(
       "parallelogramHeight"
@@ -57,20 +78,20 @@ document.getElementById("parallelogramBtn").addEventListener("click", function (
     ) {
       alert("Input is required, must be a positve number");
     } else {
-      const parallelogramArea =
-        parallelogramBaseParse * parallelogramHeightParse;
+      const parallelogramArea = (
+        parallelogramBaseParse * parallelogramHeightParse
+      ).toFixed(2);
       console.log(parallelogramArea);
     }
-
   });
 
 // step-4: Rhombus value
 document.getElementById("rhombusBtn").addEventListener("click", function () {
   const rhombusD1 = document.getElementById("rhombusD1").value;
-    const rhombusD1Parse = parseFloat(rhombusD1);
+  const rhombusD1Parse = parseFloat(rhombusD1);
   const rhombusD2 = document.getElementById("rhombusD2").value;
   const rhombusD2Parse = parseFloat(rhombusD2);
-  
+
   if (
     isNaN(rhombusD1Parse) ||
     isNaN(rhombusD2Parse) ||
@@ -79,31 +100,29 @@ document.getElementById("rhombusBtn").addEventListener("click", function () {
   ) {
     alert("Input is required, must be a positve number");
   } else {
-    const rhombusArea = 0.5 * rhombusD1Parse * rhombusD2Parse;
+    const rhombusArea = (0.5 * rhombusD1Parse * rhombusD2Parse).toFixed(2);
     // console.log(rhombusArea);
   }
-
 });
 
 // step-5: Pentagon value
 document.getElementById("pentagonBtn").addEventListener("click", function () {
   const PentagonP = document.getElementById("pentagonP").value;
-   const PentagonPParse = parseFloat(PentagonP);
+  const PentagonPParse = parseFloat(PentagonP);
   const PentagonB = document.getElementById("pentagonB").value;
   const PentagonBParse = parseFloat(PentagonB);
 
-   if (
-     isNaN(PentagonPParse) ||
-     isNaN(PentagonBParse) ||
-     PentagonP < 1 ||
-     PentagonB < 1
-   ) {
-     alert("Input is required, must be a positve number");
-   } else {
-     const PentagonArea = 0.5 * PentagonPParse * PentagonBParse;
+  if (
+    isNaN(PentagonPParse) ||
+    isNaN(PentagonBParse) ||
+    PentagonP < 1 ||
+    PentagonB < 1
+  ) {
+    alert("Input is required, must be a positve number");
+  } else {
+    const PentagonArea = (0.5 * PentagonPParse * PentagonBParse).toFixed(2);
     //  console.log(PentagonArea);
   }
-  
 });
 
 // step-6: Ellipse value
@@ -113,17 +132,16 @@ document.getElementById("ellipseBtn").addEventListener("click", function () {
   const ellipseB = document.getElementById("ellipseB").value;
   const ellipseBParse = parseFloat(ellipseB);
 
-  
-   if (
-     isNaN(ellipseAParse) ||
-     isNaN(ellipseBParse) ||
-     ellipseA < 1 ||
-     ellipseB < 1
-   ) {
-     alert("Input is required, must be a positve number");
-   } else {
-     const ellipseArea = 3.1416 * ellipseAParse * ellipseBParse;
-      console.log(ellipseArea);
-   }
- 
+  if (
+    isNaN(ellipseAParse) ||
+    isNaN(ellipseBParse) ||
+    ellipseA < 1 ||
+    ellipseB < 1
+  ) {
+    alert("Input is required, must be a positve number");
+  } else {
+    const ellipseArea = (3.1416 * ellipseAParse * ellipseBParse).toFixed(2);
+    console.log(ellipseArea);
+  }
 });
+
